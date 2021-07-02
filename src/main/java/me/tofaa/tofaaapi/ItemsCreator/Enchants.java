6ce84b6c6253b7bfb6f1ab0e1c268fd0a79ec615
Tofaa <tofik.adigozalov@gmail.com>
2021-07-02 21:26:25 +0400

    Added PlayerFunctions and Enchantment methods

---
+++ src/main/java/me/tofaa/tofaaapi/ItemsCreator/Enchants.java
@@ -0,0 +1,22 @@
+package me.tofaa.tofaaapi.ItemsCreator;
+
+
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+
+public class Enchants {
+
+    public static void addEnchant(ItemStack item, Enchantment enchantment, int level){
+        item.addUnsafeEnchantment(enchantment, level);
+    }
+
+
+    public static void removeEnchant(ItemStack item, Enchantment enchantment){
+        item.removeEnchantment(enchantment);
+    }
+
+
+
+
+}
+
--- src/main/java/me/tofaa/tofaaapi/Utils/PlayerFunctions.java
+++ src/main/java/me/tofaa/tofaaapi/Utils/PlayerFunctions.java
@@ -4,7 +4,19 @@ import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.entity.Player;
 
-public class ActionBar {
+
+public class PlayerFunctions {
+
+
+
+    public static void setXP(Player player, float xp){
+        player.setExp(xp);
+    }
+
+
+    public static void setXPLevel(Player player, int level){
+        player.setLevel(level);
+    }
 
 
     public static void sendActionbar(Player player, String message){

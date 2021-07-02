c0097e761657b7e100ed624c018348400d099dd6
Tofaa <tofik.adigozalov@gmail.com>
2021-07-02 22:11:56 +0400

    Started setting of generic attributes

--- src/main/java/me/tofaa/tofaaapi/ItemsCreator/ItemsCreator.java
+++ src/main/java/me/tofaa/tofaaapi/ItemsCreator/ItemsCreator.java
@@ -1,6 +1,8 @@
 package me.tofaa.tofaaapi.ItemsCreator;
 
 import org.bukkit.ChatColor;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -10,13 +12,17 @@ import java.util.ListIterator;
 
 public class ItemsCreator {
 
+    public static void setUnbreakable(ItemStack item, boolean unbreakable){
+        ItemMeta itemStackMeta = item.getItemMeta();
+        itemStackMeta.setUnbreakable(unbreakable);
+        item.setItemMeta(itemStackMeta);
+    }
 
     public static void setName(ItemStack item, String name) {
         ItemMeta itemStackMeta = item.getItemMeta();
         itemStackMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
         item.setItemMeta(itemStackMeta);
     }
-
 
     private static List<String> multiLineLore(String lore) {
         List<String> loreList = Arrays.asList(lore.split("\\\\n"));
@@ -26,7 +32,6 @@ public class ItemsCreator {
         }
         return loreList;
     }
-
 
     public static void setLore(ItemStack itemStack, String name) {
         String lore = ChatColor.translateAlternateColorCodes('&', name);
@@ -62,6 +67,19 @@ public class ItemsCreator {
             itemStackMeta.setLore(null);
         }
         item.setItemMeta(itemStackMeta);
+    }
+
+    public static void addDamage(ItemStack item, float damage){
+        ItemMeta itemmeta = item.getItemMeta();
+        AttributeModifier modifier = new AttributeModifier("damage", damage, AttributeModifier.Operation.ADD_NUMBER);
+        itemmeta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, modifier);
+        item.setItemMeta(itemmeta);
+    }
+    public static void addMaxHealth(ItemStack item, double hp){
+        ItemMeta itemmeta = item.getItemMeta();
+        AttributeModifier modifier = new AttributeModifier("health", hp, AttributeModifier.Operation.ADD_NUMBER);
+        itemmeta.addAttributeModifier(Attribute.GENERIC_MAX_HEALTH, modifier);
+        item.setItemMeta(itemmeta);
     }
 
 
--- src/main/java/me/tofaa/tofaaapi/Utils/PlayerFunctions.java
+++ src/main/java/me/tofaa/tofaaapi/Utils/PlayerFunctions.java
@@ -2,26 +2,30 @@ package me.tofaa.tofaaapi.Utils;
 
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 
 public class PlayerFunctions {
-
-
 
     public static void setXP(Player player, float xp){
         player.setExp(xp);
     }
 
-
     public static void setXPLevel(Player player, int level){
         player.setLevel(level);
     }
-
 
     public static void sendActionbar(Player player, String message){
 
         player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
     }
 
+    public static void setArmor(Player player, ItemStack helmet, ItemStack chestplate, ItemStack leggings, ItemStack boots){
+        player.getInventory().setHelmet(helmet);
+        player.getInventory().setHelmet(chestplate);
+        player.getInventory().setHelmet(leggings);
+        player.getInventory().setHelmet(boots);
+    }
 }
